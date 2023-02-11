68729059afa60a59244aa609c4cc95888d1cb800
zxreaper <zxreaper@foxmail.com>
2023-02-12 01:12:31 +0800

    开发RpcProvider的网络服务

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -8,6 +8,7 @@ class MprpcApplication
 public:
     static void Init(int argc, char **argv);
     static MprpcApplication &GetInstance();
+    static MprpcConfig &GetConfig();
 
 private:
     static MprpcConfig m_config;
