0f4a86d93ff76e0df39421c68c63e1fe8d896b29
zxreaper <zxreaper@foxmail.com>
2023-02-11 12:01:40 +0800

    加载配置文件部分

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "mprpcconfig.h"
+
 // mprpc框架的基础类,负责框架的一些初始化操作  设计成单例模式
 class MprpcApplication
 {
@@ -8,6 +10,8 @@ public:
     static MprpcApplication &GetInstance();
 
 private:
+    static MprpcConfig m_config;
+
     MprpcApplication() {}
     MprpcApplication(const MprpcApplication &) = delete; // 设计成单例模式，因此和拷贝相关的函数都删掉
     MprpcApplication(MprpcApplication &&) = delete;      // 设计成单例模式，因此和拷贝相关的函数都删掉
--- src/mprpcapplication.cc
+++ src/mprpcapplication.cc
@@ -2,6 +2,9 @@
 #include <iostream>
 #include <unistd.h>
 
+// 类的静态成员变量需要在类外进行初始化
+MprpcConfig MprpcApplication::m_config;
+
 void ShowArgsHelp()
 {
     std::cout << "format: commond -i <configfile>" << std::endl;
@@ -25,11 +28,9 @@ void MprpcApplication::Init(int argc, char **argv)
             config_file = optarg;
             break;
         case '?':
-            std::cout << "invalid args" << std::endl;
             ShowArgsHelp();
             exit(EXIT_FAILURE);
         case ':':
-            std::cout << "need <configfile>" << std::endl;
             ShowArgsHelp();
             exit(EXIT_FAILURE);
         default:
@@ -38,6 +39,12 @@ void MprpcApplication::Init(int argc, char **argv)
     }
 
     // 开始加载配置文件  rpcserver_ip=  rpcserver_port=  zookeeper_ip=  zookeeper_port=
+    m_config.LoadConfigFile(config_file.c_str());
+
+    // std::cout << "rpcserverip : " << m_config.Load("rpcserverip") << std::endl;
+    // std::cout << "rpcserverport : " << m_config.Load("rpcserverport") << std::endl;
+    // std::cout << "zookeeperip : " << m_config.Load("zookeeperip") << std::endl;
+    // std::cout << "zookeeperport : " << m_config.Load("zookeeperport") << std::endl;
 }
 
 MprpcApplication &MprpcApplication::GetInstance()
