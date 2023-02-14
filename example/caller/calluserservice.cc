86378cf0bd74db0d970eaf6d1714bead1c2e56b0
zxreaper <zxreaper@foxmail.com>
2023-02-15 00:25:18 +0800

    rpc调用过程

---
+++ example/caller/calluserservice.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "mprpcapplication.h"
+#include "user.pb.h"
+#include "mprpcchannel.h"
+
+int main(int argc, char **argv)
+{
+    // 整个程序启动以后，想使用mprpc框架来享受rpc服务调用，一定需要先调用框架的初始化函数（只初始化一次）
+    MprpcApplication::Init(argc, argv);
+
+    // 演示调用远程发布的rpc方法Login
+    fixbug::UserServiceRpc_Stub stub(new MprpcChannel());
+    stub.Login(); // RpcChannel->RpcChannel::callMethod 集中来做所有rpc方法调用的参数序列化和网络发送
+
+    return 0;
+}
---
+++ src/mprpcchannel.cc
@@ -0,0 +1,10 @@
+#include "mprpcchannel.h"
+
+// 所有通过stub代理对象调用的rpc方法，都走到这里了，统一做rpc方法调用的数据序列化和网络发送
+void MprpcChannel::CallMethod(const google::protobuf::MethodDescriptor *method,
+                              google::protobuf::RpcController *controller,
+                              const google::protobuf::Message *request,
+                              google::protobuf::Message *response,
+                              google::protobuf::Closure *done)
+{
+}
